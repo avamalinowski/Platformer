c616bc96bc166eff132fa426a9e29df1d3b1248c
avamalinowski <149283485+avamalinowski@users.noreply.github.com>
2025-05-26 21:49:24 -0700

    done hopefully :3
    done hopefully :3

---
+++ README.md
@@ -0,0 +1,5 @@
+Completed Platformer game made using Kenny-asset packs as well as two audio files
+One from the game terraria that acts as the background music,
+the other is a jump audio also from Kenny audio packs
+The map was created in Tiled
+one stage game where you can collect gems and find a key to unlock a chest to complete the level
---
+++ src/main.js
@@ -0,0 +1,43 @@
+// Jim Whitehead
+// Created: 4/14/2024
+// Phaser: 3.70.0
+//
+// Cubey
+//
+// An example of putting sprites on the screen using Phaser
+// 
+// Art assets from Kenny Assets "Shape Characters" set:
+// https://kenney.nl/assets/shape-characters
+
+// debug with extreme prejudice
+"use strict"
+
+// game config
+let config = {
+    parent: 'phaser-game',
+    type: Phaser.CANVAS,
+    tileBias: 32,
+    render: {
+        pixelArt: true  // prevent pixel art from getting blurred when scaled
+    },
+    physics: {
+        default: 'arcade',
+        arcade: {
+            tileBias: 32,
+            debug: true,
+            gravity: {
+                x: 0,
+                y: 0
+            }
+        }
+    },
+    width: 1925,
+    height: 500,
+    scene: [Load, Platformer]
+}
+
+var cursors;
+const SCALE = 3.0;
+var my = {sprite: {}, text: {}};
+
+const game = new Phaser.Game(config);
